ed86ed49736aac20a56d4583e5ab6ac211fd96dd
anonymous anonymous <vanessamayliu@gmail.com>
2022-03-10 22:51:15 +0000

    Whitelist columns in Nonprofit for ActiveAdmin

--- app/admin/nonprofits.rb
+++ app/admin/nonprofits.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Nonprofit do
 
+ permit_params :name, :category, :mission, :contact_name, :contact_email, :contact_phone, :owner_user_id, :status, :notes
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
